3fbcd71f71924f61dd3a84da5cf751a6f0cc3d8c
ThomasPrins <thomas.prins1@hotmail.com>
2025-06-09 16:13:18 +0200

    MILP done (matlab constraints suck + delta_u prob)

--- Hybrid_Systems_Assignment.m
+++ Hybrid_Systems_Assignment.m
@@ -5,19 +5,19 @@ x_max = 40;   % mm
 N_grind_max = 10;
 psi = 0.98;
 psi_real = 1.09;
-gamma0 = 0;
-gamma1 = 1;
-gamma2 = 3;
+gamma1 = 0;
+gamma2 = 1;
+gamma3 = 3;
+gamma = [gamma1,gamma2,gamma3];
 
 % max variables:
 j_max = 5; % number of sections
 q_max = 3; % number of splits in a section
 iter_max = 100; % number of iterations
-%track_max = 70;
-N_p = 10; % prediction horizon (in months?)
+N_p = 6; % prediction horizon (in months?)
 
 grp_nr = 4;
-x_con = zeros(j_max,iter_max);
+x_con = zeros(j_max,iter_max+N_p);
 x_con(:,1) = [20+0.5*(abs(grp_nr-15));
      22+0.5*(abs(grp_nr-15));
      25+0.5*(abs(grp_nr-15));
@@ -46,10 +46,10 @@ f_degrade = zeros(j_max,1);
 f_grind = zeros(j_max,1);
 
 % MLD
-delta = np.zeros(2,1);
+delta = zeros(3,1);
 
 % cost_function
-%J = J_deg + lapda*J_maint;
+% J = J_deg + lapda*J_maint;
 % J_deg = sum_j(sum_l(x_j_con)) summation of the length of degradation spots
 % J_maint = sum_j(sum_l(sum_q(gamma*I))) 
 % where I is an indication function to check if k+l-1 = a_q, its 1 if true,
@@ -59,15 +59,123 @@ delta = np.zeros(2,1);
 % dynamics:
 
 %% Functions
-function out = predictTrackDeg(track,weightsA,weightsB)
-    if track<30
-        out = weightsA(1)*track+weightsB(1);
-    elseif track<50
-        out = weightsA(2)*track+weightsB(2);
-    elseif track<70
-        out = weightsA(3)*track+weightsB(3);
-    else
-        out = 0;
+function out = convexMILP(x_con,lapda,gamma,n,A,B,x_eff,psi)
+    delta_u = optimvar('delta_u', 3, 1, 'Type', 'integer', 'LowerBound', 0, 'UpperBound', 1);
+    delta = optimvar('delta', 3, 'Type', 'integer', 'LowerBound', 0, 'UpperBound', 1);
+    z = optimvar('z', 3, 'LowerBound', 0);
+
+    x_min = [0,30,50];
+    x_max = [30,50,70];
+    ineq_constraints = [];
+    eq_constraints = [];
+    eq_constraints = [eq_constraints; sum(delta_u) == 1];
+    ineq_constraints = [ineq_constraints;
+        -delta(1) + delta(3) <= 0;
+        -delta(2) + delta(3) <= 0;
+        delta(1) + delta(2) - delta(3) <= 1;
+    ];
+    
+    for i = 1:3
+        ineq_constraints = [ineq_constraints;
+            z(i) <= x_max(i) * delta(i);
+            z(i) >= x_min(i) * delta(i);
+        ];
+    end
+    % u is also a binary value that we want to use inside our convex 
+    % optimization, so this also requires a auxilary variable to decide if
+    % MPC will choose to do maintenance, replace or do nothing
+    for j = 1:n
+        for l = 1:N_p
+            for i = 1:3
+                ineq_constraints = [ineq_constraints;
+                z(i) <= x_con(j,k+l-1) - x_min(i) * (1 - delta(i));
+                z(i) >= x_con(j,k+l-1) - x_max(i) * (1 - delta(i));
+                ];
+            end
+            track_prediction = predictTrackDeg(x_con(j,k+l-1),A,B,delta,z,"MLD");
+            grinding_prediction = predictGrindingEffect(x_con(j,k+l-1),psi,x_eff);
+            x_con(j,k+l) = delta_u(1)*track_prediction + delta_u(2)*grinding_prediction + delta_u(3)*0;
+
+        end
+    end
+    J_deg_pred = sum(x_con);
+
+    for j = 1:n
+        for l = 1:N_p
+            for q = 1:p
+                if delta_u(q) == q
+                    J_maint = J_maint + gamma(q);
+                    % assigment says gamma(j,q), but i cannot see how gamma is different between tracks?
+                    % This would onyl be a small change anyway, but gamma
+                    % values are only given for q=1,q=2 and q=3, without
+                    % differences between j values
+                else
+                    J_maint = J_maint + 0;
+                end
+            end
+        end
+    end
+    J_maint_lapda = lapda*J_maint;
+
+    J = J_deg_pred + J_maint_lapda;
+
+    prob = optimproblem;
+    prob.Objective = J;
+    prob.Constraints.eq = eq_constraints;
+    prob.Constraints.ineq = ineq_constraints;
+    options = optimoptions('intlinprog', 'Display', 'iter'); % Show solver output
+    %int_vars = [delta_u; delta];
+    % Solve the problem
+    [sol, fval, exitflag, output] = solve(prob, x0, 'Options', options, 'Solver', 'intlinprog');
+    [~, u] = max(sol.delta_u); 
+    out = u - 1; % Because MATLAB indices start at 1, so subtract 1 to get u ∈ {0,1,2}
+end
+
+function out = predictTrackDeg(track,weightsA,weightsB,delta,z,controller_type)
+    if controller_type == "PWA"
+        if track<30
+            out = weightsA(1)*track+weightsB(1);
+        elseif track<50
+            out = weightsA(2)*track+weightsB(2);
+        elseif track<70
+            out = weightsA(3)*track+weightsB(3);
+        else
+            out = 0;
+        end
+    elseif controller_type == "MLD"
+        % fully written out the following needs to hold:
+        %dynamic1 = ((1-delta(1))*(1-delta(2))*weightsA(1)*track+weightsB(1));
+        %dynamic2 = (delta(1)*(1-delta(2))*weightsA(2)*track+weightsB(2));
+        %dynamic3 = ((1-delta(1))*delta(2)*weightsA(3)*track+weightsB(3));
+        % This results in terms with delta1*delta2, which we set equal to
+        % delta3 under the following conditions:
+        % -delta1+delta3 <= 0
+        % -delta1+delta3 <= 0
+        % delta1 + delta2 - delta3 <= 1
+        delta(3) = delta(1)*delta(2); % under constraints!
+        %dynamic1 = (1+delta3-delta(1)-delta(2)*weightsA(1)*track+weightsB(1));
+        %dynamic2 = ((delta(2)-delta(3))*weightsA(2)*track+weightsB(2));
+        %dynamic3 = ((delta(1)-delta(3))*weightsA(3)*track+weightsB(3));
+        % Then the next step will be to take out the delta1*x and delta2*x
+        % and replace this with auxillary variables z1 and z2, this will
+        % cause the function to be represented by z, delta, x all in
+        % liniear form:
+        z1 = delta(1)*track;
+        z2 = delta(2)*track;
+        z3 = delta(3)*track;
+        z = [z1,z2,z3]
+        dynamic1A = -z(1)*weightsA(1)-z(2)*weightsA(1)+z(3)*weightsA(1)+track*weightsA(1);
+        dynamic1B = -delta(1)*weightsB(1)-delta(2)*weightsB(1)+delta(3)*weightsB(1)+weightsB(1);
+        dynamic2 = z(2)*weightsA(2) - z(3)*weightsA(2) + delta(2)*weightsB(2) - delta(3)*weightsB(2);
+        dynamic3 = z(1)*weightsA(3) - z(3)*weightsA(3) + delta(1)*weightsB(3) - delta(3)*weightsB(3);
+        % Now z also needs to hold for some conditions:
+        % z <= max(x)*delta
+        % z >= min(x)*delta
+        % z <= x - min(x)(1-delta)
+        % z >= x - max(x)(1-delta)
+        out = dynamic1A + dynamic1B + dynamic2 + dynamic3;
+        % 4th dynamic with delta1 = 1 && delta 2 equal to 1 will just be
+        % equal to 0
     end
 end
 
@@ -77,30 +185,6 @@ function out = predictGrindingEffect(track,psi_f,x_eff_f)
     else
         out = psi_f*(track-x_eff_f); % grinding possible
     end
-end
-
-%% ! this part is no longer needed I believe!
-for j = 1:j_max
-    if x_con(j,1)<30
-        %x_con_split(j,1) = x_con(j,1);
-        f_degrade(j) = A(1)*x_con(j,1)+B(1);
-    elseif x_con(j,1)<50
-        %x_con_split(j,2) = x_con(j,1);
-        f_degrade(j) = A(2)*x_con(j,1)+B(2);
-    elseif x_con(j,1)<70
-        %x_con_split(j,3) = x_con(j,1);
-        f_degrade(j) = A(3)*x_con(j,1)+B(3);
-    end
-    % maybe we need this instead?
-    %for q = 1:q_max
-        %f_degrade(j,q) = A(q)*x_con_split(j,q)+B(q);
-    %end
-    if x_con(j,1) <= x_eff
-        f_grind = 0; % no grinding possible
-    else
-        f_grind = psi*(x_con(j,1)-x_eff); % grinding possible
-    end
-% end of initialization
 end
 
 %% Main code
@@ -114,11 +198,11 @@ end
 for k = 1:iter_max % k is iterations so need to be reworked into other var
     for j = 1:j_max
         % set degradation dynamics for each different value of u
-        if u(j)== 0
-            f_degrade(j) = predictTrackDeg(x_con(j,k),A,B)
+        if u(j)== 0 
+            f_degrade(j) = predictTrackDeg(x_con(j,k),A,B,delta,[0,0,0],"MLD")
             x_con(j,k+1) = f_degrade(j);
         elseif u(j) == 1
-            f_grind(j) = predictGrindingEffect(x_con(j,k),psi,x_eff)
+            f_grind(j) = predictGrindingEffect(x_con(j,k),psi,x_eff) % constant so nothing needed
             x_con(j,k+1) = f_grind(j);
         elseif u(j) == 2 % replace
             x_con(j,k+1) = 0;
@@ -132,10 +216,13 @@ for k = 1:iter_max % k is iterations so need to be reworked into other var
             x_aux(j,k+1) = 0;
         end
     end
-    for l = 1:N_p
-        % here we make our prediction based on l future iterations
-        
-    end
+
+    % MPC part:
+    % lapda = linspace(600,800,50)
+    % for r = 1:51 ...
+    lapda = 600; % needs to be between 600-800
+    u(k) = convexMILP(x_con,lapda,gamma,j_max,A,B,x_eff,psi)
+    %predictTrackDeg(x_con(j,k),A,B,delta,"MLD")
 end
 % l is the prediction horizon to look into the future
 % our system is now a PWA with:
@@ -152,10 +239,13 @@ end
 % [0 1] = no state, make sure to exclude (by putting to 0?)
 % also done here: https://www.researchgate.net/publication/262220797_MLD_Systems_Modeling_and_Control
 % We create an extended state space: x(k) = [x(k), delta1, delta2]
-% if delta1 = 0 & delta2 = 0: A1 + B1*u + 0 + 0
-% if delta1 = 1 & delta2 = 0: A1 + A2 + B1*u + B2 + 0
-% if delta1 = 0 & delta2 = 1: A1 + A3 + B1*u + B3 + 0
-A123 + B1 A2+B2 A3+B3
+% if delta1 = 0 & delta2 = 0: A1*x + B1*u + 0 + 0
+% if delta1 = 1 & delta2 = 0: A1 + A2 + B1*u + B2 + 0 = A2 + B2*u
+% if delta1 = 0 & delta2 = 1: A1 + A3 + B1*u + B3 + 0 = A3 + B3*u
+% A123 + B1 A2+B2 A3+B3
+
+% Furthermore, our control input is also discrete.
+% 
 %% Plotting
 x_con_split;
 f_degrade;
